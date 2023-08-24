cdae30c59e41c0e4a473eb28261cceb2711564cc
hsikai <hsikai.wang@ruckuswireless.com>
2023-08-25 02:06:15 +0800

    add locked

--- kernel/lkmex/04-file-op/hc_op_locked.c
+++ kernel/lkmex/04-file-op/hc_op_locked.c
@@ -1,20 +1,30 @@
 /*
-	container_of()
-	copy_to_user()
-	copy_from_user()
-	struct inode
-	struct file
+	sema_init()
+	down_interruptible()
+	up()
+	mutex_init()
+	mutex_lock_interruptible()
+	mutex_unlock()
+
 */
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/slab.h>
-
 #include <linux/uaccess.h>
+#include <linux/jiffies.h>
+#include <linux/sched.h>
+
+#include <linux/semaphore.h>
+#include <linux/mutex.h>
+
+#ifndef LOCK_USE
+#define LOCK_USE 1 // 0:semaphore,1:mutex
+#endif
 
 #define HELLO_MAJOR 0
 #define HELLO_NR_DEVS 2
-#define DEV_NAME "hc_op"
+#define DEV_NAME "hc_op_locked"
 
 int hello_major = HELLO_MAJOR;
 int hello_minor = 0;
@@ -32,6 +42,8 @@ struct hello_char_dev
 	struct cdev cdev;
 	char *c;
 	int n;
+	struct semaphore sema;
+	struct mutex mtx;
 };
 
 struct hello_char_dev *hc_devp;
@@ -40,7 +52,7 @@ struct class *hc_cls;
 int hc_open(struct inode *inode, struct file *filp)
 {
 	struct hello_char_dev *hc_dev;
-	printk(KERN_INFO "open %s%d %d\n", DEV_NAME, iminor(inode), MINOR(inode->i_cdev->dev));
+	printk(KERN_INFO "%s %s open \n", DEV_NAME, current->comm);
 	hc_dev = container_of(inode->i_cdev, struct hello_char_dev, cdev);
 	filp->private_data = hc_dev;
 
@@ -50,7 +62,7 @@ ssize_t hc_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos
 {
 	ssize_t retval = 0;
 	struct hello_char_dev *hc_dev = filp->private_data;
-	printk(KERN_INFO "read %s %p\n", DEV_NAME, hc_dev);
+	printk(KERN_INFO "%s read %p\n", DEV_NAME, hc_dev);
 
 	if (*f_pos >= hc_dev->n)
 		goto out;
@@ -71,34 +83,66 @@ out:
 ssize_t hc_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
 {
 	struct hello_char_dev *hc_dev = filp->private_data;
-	int retval = -ENOMEM, i;
-	printk(KERN_INFO "write %s %p\n", DEV_NAME, hc_dev);
+	int retval = -ENOMEM;
+	unsigned long jiff1;
+	printk(KERN_INFO "%s write begin\n", current->comm);
+#if (LOCK_USE == 0)
+	if (down_interruptible(&hc_dev->sema)) //-EINTR
+		return -ERESTARTSYS;
+	printk(KERN_INFO "%s get sema\n", current->comm);
+#endif
+#if (LOCK_USE == 1)
+	if (mutex_lock_interruptible(&hc_dev->mtx)) //-EINTR
+		return -ERESTARTSYS;
+	printk(KERN_INFO "%s get mutex\n", current->comm);
+#endif
+
 	kfree(hc_dev->c);
 	hc_dev->c = NULL;
 	hc_dev->n = 0;
+	printk(KERN_INFO "%s 1", current->comm);
+	jiff1 = jiffies;
+	while (jiffies - jiff1 < HZ)
+		;
 	hc_dev->c = kzalloc(count, GFP_KERNEL);
-	printk(KERN_INFO "addr %p", hc_dev->c);
 	if (!hc_dev->c)
 		goto out;
+	printk(KERN_INFO "%s 2 addr:%p", current->comm, hc_dev->c);
+	jiff1 = jiffies;
+	while (jiffies - jiff1 < HZ)
+		;
+	printk(KERN_INFO "%s %s 3 addr:%p", DEV_NAME, current->comm, hc_dev->c);
 	if (copy_from_user(hc_dev->c, buf, count))
 	{
 		retval = -EFAULT;
 		goto fail_copy;
 	}
-	for (i = 0; i < count; i++)
-		printk(KERN_INFO "get %c", hc_dev->c[i]);
 	hc_dev->n = count;
 
+#if (LOCK_USE == 0)
+	up(&hc_dev->sema);
+	printk(KERN_INFO "%s up sema\n", current->comm);
+#endif
+#if (LOCK_USE == 1)
+	mutex_unlock(&hc_dev->mtx);
+	printk(KERN_INFO "%s unlock mutex\n", current->comm);
+#endif
+	printk(KERN_INFO "%s %s write done", DEV_NAME, current->comm);
 	return count;
 fail_copy:
 	kfree(hc_dev->c);
 out:
+#if (LOCK_USE == 0)
+	up(&hc_dev->sema);
+#endif
+#if (LOCK_USE == 1)
+	mutex_unlock(&hc_dev->mtx);
+#endif
 	return retval;
 }
-
 int hc_release(struct inode *inode, struct file *filp)
 {
-	printk(KERN_INFO "release %s\n", DEV_NAME);
+	printk(KERN_INFO "%s %s release\n", DEV_NAME, current->comm);
 	return 0;
 }
 
@@ -140,6 +184,12 @@ static int __init hello_init(void)
 
 	for (i = 0; i < hello_nr_devs; i++)
 	{
+#if (LOCK_USE == 0)
+		sema_init(&hc_devp[i].sema, 1);
+#elif (LOCK_USE == 1)
+		mutex_init(&hc_devp[i].mtx);
+#endif
+
 		cdev_init(&hc_devp[i].cdev, &hc_fops);
 		hc_devp[i].cdev.owner = THIS_MODULE;
 		ret = cdev_add(&hc_devp[i].cdev, MKDEV(hello_major, hello_minor + i), 1);
